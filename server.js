ef7546f4e293c96e3cee99e4d9d5d93990fb2c31
Lisa Lee <unclily@live.unc.edu>
2022-09-01 15:31:16 -0400

    a01 fifth commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Require http module
-const http = require('http');
+const http = require('http')
 
 // Require fs module
-const fs = require('fs');
+const fs = require('fs')
 
 // Require minimist module (make sure you install this one via npm).
-const mini = require('minimist')(process.argv.slice(2));
+const mini = require('minimist')(process.argv.slice(2))
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
@@ -17,16 +17,15 @@ const port = args.port || 3000
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-const fileName = './public/index.html';
+const fileName = './public/index.html'
 fs.readFile(fileName, (err, data) => {
       if (err) {
- //       console.log(err);
-        //return;
-      console.error('There was an uncaught error', err);
-      process.exit(1);
+        console.log(err)
+ //     console.error('There was an uncaught error', err)
+ //     process.exit(1)
       }
-      //console.log(data);
-    });
+      //console.log(data)
+    })
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
@@ -45,7 +44,7 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html')
       res.end(data)
-});
+})
 
 
 
@@ -53,8 +52,8 @@ const server = http.createServer((req, res) => {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
-      console.log('Server listening on port ${port}');
-});
+      console.log('Server listening on port ${port}')
+})
 
 
 
